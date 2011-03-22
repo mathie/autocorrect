b427a0090c52e4f47b9f04d8aff2ba7e942cd169
Graeme Mathieson <mathie@woss.name>
2011-03-22 20:23:19 +0000

    Autocorrect with suggestions.

--- autocorrect.gemspec
+++ autocorrect.gemspec
@@ -6,13 +6,15 @@ Gem::Specification.new do |s|
   s.name        = "autocorrect"
   s.version     = Autocorrect::VERSION
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["TODO: Write your name"]
-  s.email       = ["TODO: Write your email address"]
-  s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
+  s.authors     = ["Graeme Mathieson"]
+  s.email       = ["mathie@woss.name"]
+  s.homepage    = "http://github.com/mathie/autocorrect"
+  s.summary     = %q{If you can't spell, or type, your method names we're here to help.}
+  s.description = %q{A crazy gem that 'corrects' your misspelling of method names. Please don't actually use it, I was just testing an idea in my head!}
 
   s.rubyforge_project = "autocorrect"
+
+  s.add_dependency 'text'
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
--- lib/autocorrect/whiny.rb
+++ lib/autocorrect/whiny.rb
@@ -1,34 +1,53 @@
+require 'text/levenshtein'
+
 module Autocorrect
   module Whiny
     def method_missing(method, *args, &block)
       if method == :to_ary || method == :to_str
         super
-      elsif guessed_method = guess_callable_method_for(method, args.length, block_given?)
-        raise_method_missing_warning_for guessed_method, method, caller, args.empty? ? nil : args, block_given?
+      elsif guessed_method = guess_callable_method_for(method, args.length)
+        raise_method_missing_warning_for guessed_method, method, args, caller
       elsif guessed_method = guess_closest_matched_name_method_for(method)
-        raise_method_missing_warning_for guessed_method, method, caller
+        raise_method_missing_with_mismatched_args_warning_for guessed_method, method, args, caller
       else
         super
       end
     end
 
-    def guess_callable_method_for(method, arity, block_supplied)
-      "horses"
+    def guess_callable_method_for(method, arity)
+      nearest_methods(method).find { |possible_method| [arity, -1].include? method(possible_method).arity }
     end
 
     def guess_closest_matched_name_method_for(method)
-      "kangaroos"
+      nearest_methods(method)[0]
     end
 
-    def raise_method_missing_warning_for(guessed_method, called_method, with_caller = nil, args = nil, block_present = false)
+    def nearest_methods(method)
+      potential_methods = methods_grouped_by_distance(method)
+      nearest_distance = potential_methods.keys.sort[0]
+      potential_methods[nearest_distance]
+    end
+
+    def methods_grouped_by_distance(method)
+      methods.group_by { |potential_method| Text::Levenshtein.distance(potential_method.to_s, method.to_s) }
+    end
+
+    def raise_method_missing_warning_for(guessed_method, called_method, args, with_caller = nil)
       message = "You have called a method that doesn't exist.\n"
-      message << "You tried to call #{print_method_with_args(called_method, args, block_present)}.\n"
-      message << "We suspect you meant #{print_method_with_args(guessed_method, args, block_present)}.\n"
+      message << "You tried to call '#{print_method_with_args(called_method, args)}'.\n"
+      message << "We suspect you meant '#{print_method_with_args(guessed_method, args)}'.\n"
       raise NoMethodError, message, with_caller || caller
     end
 
-    def print_method_with_args(method, args = nil, block_present = false)
-      "#{method.to_s}(#{args.nil? ? '' : args.map { |arg| arg.inspect }.join(", ")}#{block_present ? ", &block" : ""})"
+    def raise_method_missing_with_mismatched_args_warning_for(guessed_method, called_method, args, block_present, with_caller = nil)
+      message = "You have called a method that doesn't exist.\n"
+      message << "You tried to call '#{print_method_with_args(called_method, args)}'.\n"
+      message << "We suspect you meant '#{guessed_method}' but you got the number of arguments wrong too (#{method(guessed_method).arity} expected, got #{args.length}).\n"
+      raise NoMethodError, message, with_caller || caller
+    end
+
+    def print_method_with_args(method, args)
+      "#{method.to_s}(#{args.map { |arg| arg.inspect }.join(", ")})"
     end
   end
 end
